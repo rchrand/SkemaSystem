76b69676c06b7cf025953ebe33a4cd808550a103
Rune Andersen <Rune.Andersen92@gmail.com>
2014-05-11 19:48:54 +0200

    Added a database test on the frontpage

--- SkemaSystem/Controllers/HomeController.cs
+++ SkemaSystem/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -10,7 +12,22 @@ namespace SkemaSystem.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var sqlCon = new SqlConnection(ConfigurationManager.ConnectionStrings["AppHarborDB"].ConnectionString);
+            List<String> strings = new List<String>();
+            var sql = "SELECT * FROM test";
+
+            sqlCon.Open();
+            var cmd = new SqlCommand(sql, sqlCon);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                strings.Add(reader["test"] as String);
+            }
+
+            sqlCon.Close();
+            reader.Close();
+            return View(strings);
         }
 
         public ActionResult About()
