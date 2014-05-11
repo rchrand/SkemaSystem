4a69f9baaff7f5cbe31612bb00ce84673f1e3ec6
Rune Andersen <Rune.Andersen92@gmail.com>
2014-05-11 17:34:44 +0200

    Removed some text and actions, replaced it with some random text

--- SkemaSystem/Controllers/HomeController.cs
+++ SkemaSystem/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace SkemaSystem.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
