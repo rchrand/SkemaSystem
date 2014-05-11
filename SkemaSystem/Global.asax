8bfb3081741b61b8d4a35aa66f8e299da13463b7
Rune Andersen <Rune.Andersen92@gmail.com>
2014-05-11 17:25:39 +0200

    Intial commit v3

---
+++ SkemaSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SkemaSystem.MvcApplication" Language="C#" %>
---
+++ SkemaSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkemaSystem.Startup))]
+namespace SkemaSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
