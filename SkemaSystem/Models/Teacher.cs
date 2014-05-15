e7154d200c8adb4b0531ab679b17ab75a4c36bd1
JPA <killerko@msn.com>
2014-05-15 12:53:21 +0200

    added some more modelclasses

--- SkemaSystem/Models/Teacher.cs
+++ SkemaSystem/Models/Teacher.cs
@@ -2,12 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace SkemaSystem.Models
 {
     public class Teacher
     {
+        [Required]
         public int Id { get; set; }
+
+        [Required]
         public string Name { get; set; }
     }
 }
