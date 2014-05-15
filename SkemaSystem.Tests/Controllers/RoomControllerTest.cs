bc820cf84833912bbe89f80606309e88a3d6574e
JPA <killerko@msn.com>
2014-05-15 13:35:38 +0200

    Forgot the file

---
+++ SkemaSystem.Tests/Controllers/RoomControllerTest.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SkemaSystem;
+using SkemaSystem.Controllers;
+
+namespace SkemaSystem.Tests.Controllers
+{
+    [TestClass]
+    public class RoomControllerTest
+    {
+        [TestMethod]
+        public void Rooms()
+        {
+            //Arrange
+            RoomController controller = new RoomController();
+
+            //Act
+            ViewResult result = controller.Rooms() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void CreateRoom()
+        {
+            //Arrange
+            RoomController controller = new RoomController();
+
+            //Act
+            ViewResult result = controller.CreateRoom() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void DeleteRoom()
+        {
+            //Arrange
+            RoomController controller = new RoomController();
+
+            //Act
+            ViewResult result = controller.DeleteRoom() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void UpdateRoom()
+        {
+            //Arrange
+            RoomController controller = new RoomController();
+
+            //Act
+            ViewResult result = controller.UpdateRoom() as ViewResult;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+    }
+}
