39466a1d0636ce845093394db5925601d74209c6
Benjamin Kock <ceo@prowebster.dk>
2014-05-19 20:48:18 +0200

    TestCreated

---
+++ SkemaSystem.Tests/Features/ClassesTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SkemaSystem.Tests.Features
+{
+    [TestClass]
+    public class ClassesTest
+    {
+        [TestMethod]
+        public void ClassesTest()
+        {
+        }
+    }
+}
