3f73b440344e407e84321a49b457307ffbf53e86
Saftemand <michaelmantonsen@gmail.com>
2014-05-19 12:44:44 +0200

    Bugfix with fakedb fixed

--- SkemaSystem/Controllers/BaseController.cs
+++ SkemaSystem/Controllers/BaseController.cs
@@ -1,6 +1,7 @@
 ﻿using SkemaSystem.Models;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,13 +10,17 @@ namespace SkemaSystem.Controllers
 {
     public class BaseController : Controller
     {
-        SkeamSystemDb _db = new SkeamSystemDb();
+        ISkemaSystemDb _db;
         public IEnumerable<Education> _educationModel;
 
         public BaseController()
         {
-            _educationModel = _db.Educations.ToList();
-             ViewBag.EducationModel = _educationModel;
+            _db = new SkeamSystemDb();
+            if (ConfigurationManager.ConnectionStrings["skeamsysdb"] != null)
+            {
+                _educationModel = _db.Query<Education>().ToList();
+                ViewBag.EducationModel = _educationModel;
+            }
         }
 	}
 }
