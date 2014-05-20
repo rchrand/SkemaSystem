5555dbf5df4e2beafdc71444f6f1f62a13768dd8
Rune Andersen <Rune.Andersen92@gmail.com>
2014-05-20 09:02:57 +0200

    Added the test file

---
+++ SkemaSystem.Tests/Controllers/UserControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SkemaSystem.Tests.Controllers
+{
+    [TestClass]
+    public class UserControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+        }
+    }
+}
