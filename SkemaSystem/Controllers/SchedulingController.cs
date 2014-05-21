1eb6b2028cae818162d661ba03455fdb4c8389a7
Rune Andersen <Rune.Andersen92@gmail.com>
2014-05-21 14:54:15 +0200

    Added the scheduling contller and started the view

---
+++ SkemaSystem/Controllers/SchedulingController.cs
@@ -0,0 +1,26 @@
+﻿using SkemaSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SkemaSystem.Controllers
+{
+    [RouteArea("admin")]
+    [RoutePrefix("scheduling")]
+    [Route("{action=index}")]
+    public class SchedulingController : BaseController
+    {
+        public ActionResult Index()
+        {
+            var db = new SkeamSystemDb();
+
+            IEnumerable<SelectListItem> educations = from s in db.Educations
+                                                    select new SelectListItem { Text = s.Name, Value = s.Id };
+
+            ViewBag._educations = educations;
+            return View();
+        }
+	}
+}
