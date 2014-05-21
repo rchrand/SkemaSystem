1579e1c530fdf4be935268cbc28c84067375a8f5
Saftemand <michaelmantonsen@gmail.com>
2014-05-21 13:24:47 +0200

    switching branch

---
+++ SkemaSystem/Models/ViewModels/EducationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SkemaSystem.Models.ViewModels
+{
+    public class EducationViewModel
+    {
+        public Education Education { get; set; }
+        public IEnumerable<Teacher> AvailableTeachers { get; set; }
+        public IEnumerable<Teacher> SelectedTeachers { get; set; }
+        public PostedTeachers PostedTeachers { get; set; }
+    }
+}
