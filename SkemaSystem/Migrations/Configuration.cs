c2fbae94603b4613afdc28d534e0f3806a10246e
Rune Andersen <Rune.Andersen92@gmail.com>
2014-05-23 10:45:59 +0200

    Added the data loss thing, since its important for appHarbor and the system

--- SkemaSystem/Migrations/Configuration.cs
+++ SkemaSystem/Migrations/Configuration.cs
@@ -11,6 +11,7 @@ namespace SkemaSystem.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(SkemaSystem.Models.SkeamSystemDb context)
